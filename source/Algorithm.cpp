5abff7affe6da38cfeb9fd6e2757494f6f24053d
Al1nL <alinl@mail.tau.ac.il>
2025-04-19 13:48:48 +0300

    chnage

--- source/Algorithm.cpp
+++ source/Algorithm.cpp
@@ -1,50 +1,23 @@
+//
+// Created by admin on 4/18/2025.
+//
+
 #include "../headers/Algorithm.h"
 
+Direction Algorithm::calculateDirection(int currRow, int currCol, int targetRow, int targetCol) {
+    if (currRow < targetRow && currCol == targetCol) return Direction::D;
+    if (currRow > targetRow && currCol == targetCol) return Direction::U;
+    if (currRow == targetRow && currCol < targetCol) return Direction::R;
+    if (currRow == targetRow && currCol > targetCol) return Direction::L;
+    if (currRow < targetRow && currCol < targetCol) return Direction::DR;
+    if (currRow < targetRow && currCol > targetCol) return Direction::DL;
+    if (currRow > targetRow && currCol < targetCol) return Direction::UR;
+    if (currRow > targetRow && currCol > targetCol) return Direction::UL;
 
-vector<pair<int, int>> WinAlgorithm::getNeighbors(int x, int y) {
-    return {{x - 1, y}, {x + 1, y}, {x, y - 1}, {x, y + 1}};
+    return player->getDir(); // Default to current direction if no match
 }
 
-Action WinAlgorithm::nextMove() {
-    auto [currentRow, currentCol] = player->getPos();
-    auto [targetRow, targetCol] = opponent->getPos();
-
-    Movable::Direction currentDir = player->getDir();
-	Movable::Direction desiredDir = calculateDirection(currentRow, currentCol, targetRow, targetCol);
-
-    // Rotate if not aligned
-    if (currentDir != desiredDir) {
-        return determineRotation(currentDir, desiredDir);
-    }
-
-    // Move forward if aligned
-    if (currentRow != targetRow || currentCol != targetCol) {
-        return Action::MoveFwd;
-    }
-
-    // Shoot if aligned with the target
-    if (player->getRemainingShells() > 0) {
-        return Action::Shoot;
-    }
-
-    // Default fallback action
-    return Action::Rotate1_8Right;
-}
-
-Movable::Direction Algorithm::calculateDirection(int currRow, int currCol, int targetRow, int targetCol) {
-        if (currRow < targetRow && currCol == targetCol) return Movable::Direction::D;
-        if (currRow > targetRow && currCol == targetCol) return Movable::Direction::U;
-        if (currRow == targetRow && currCol < targetCol) return Movable::Direction::R;
-        if (currRow == targetRow && currCol > targetCol) return Movable::Direction::L;
-        if (currRow < targetRow && currCol < targetCol) return Movable::Direction::DR;
-        if (currRow < targetRow && currCol > targetCol) return Movable::Direction::DL;
-        if (currRow > targetRow && currCol < targetCol) return Movable::Direction::UR;
-        if (currRow > targetRow && currCol > targetCol) return Movable::Direction::UL;
-
-        return player->getDir(); // Default to current direction if no match
-    }
-
-Action Algorithm::determineRotation(Movable::Direction currentDir, Movable::Direction desiredDir) {
+Action Algorithm::determineRotation(Direction currentDir, Direction desiredDir) {
     int rotationSteps = (static_cast<int>(desiredDir) - static_cast<int>(currentDir) + 8) % 8;
     if (rotationSteps == 1 || rotationSteps == 2) return Action::Rotate1_8Right;
     if (rotationSteps == 6 || rotationSteps == 7) return Action::Rotate1_8Left;
